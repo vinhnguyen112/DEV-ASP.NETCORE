349d284448e4a155401e9ee6893051d237ee1ea9
vinhnguyen112 <vinhnguyen112@gmail.com>
2026-09-15 20:45:04 +0700

    Wb6

---
+++ WB6/WB6/Controllers/NTVMemberController.cs
@@ -0,0 +1,89 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
+using WB6.Models.DataModels;
+
+namespace WB6.Controllers
+{
+    public class NTVMemberController : Controller
+    {
+        private static readonly List<NTVMember> _members = new List<NTVMember>()
+            {
+                new NTVMember
+    {
+        NTVMemberId = Guid.NewGuid().ToString(),
+        NTVUserName = "nguyenvana",
+        NTVEmail = "vana@gmail.com",
+        NTVFullName = "Nguyen Van A",
+        NTVPassword = "Password@123"
+    },
+                new NTVMember
+    {
+        NTVMemberId = Guid.NewGuid().ToString(),
+        NTVUserName = "tranthib",
+        NTVEmail = "thib@gmail.com",
+        NTVFullName = "Tran Thi B",
+        NTVPassword = "Password@456"
+    },
+                new NTVMember
+    {
+        NTVMemberId = Guid.NewGuid().ToString(),
+        NTVUserName = "levanc",
+        NTVEmail = "vanc@gmail.com",
+        NTVFullName = "Le Van C",
+        NTVPassword = "Password@789"
+    },
+                new NTVMember
+    {
+        NTVMemberId = Guid.NewGuid().ToString(),
+        NTVUserName = "phamthid",
+        NTVEmail = "thid@gmail.com",
+        NTVFullName = "Pham Thi D",
+        NTVPassword = "Password@321"
+    },
+                new NTVMember
+    {
+        NTVMemberId = Guid.NewGuid().ToString(),
+        NTVUserName = "hoangvane",
+        NTVEmail = "vane@gmail.com",
+        NTVFullName = "Hoang Van E",
+        NTVPassword = "Password@654"
+    }
+
+            };
+        public IActionResult Index()
+        {
+            return View(_members);
+        }
+        public IActionResult GetMember()
+        {
+            // Here you would typically fetch the NTVMember details from a database or service using the provided id.
+            // For demonstration purposes, we'll create a dummy NTVMember object.
+            var member = new NTVMember();
+            member.NTVMemberId = Guid.NewGuid().ToString();
+            member.NTVUserName = "SampleUserName";
+            member.NTVEmail = "sample@example.com";
+            member.NTVFullName = "Sample Full Name";
+            member.NTVPassword = "SamplePassword";
+
+            ViewBag.NTVmember = member;
+            return View(member);
+        }
+
+        public ActionResult Create()
+        {
+            var member = new NTVMember();
+            return View(member);
+        }
+
+        [HttpPost]
+        public ActionResult Create(NTVMember member)
+        {
+           member.NTVMemberId = Guid.NewGuid().ToString();
+            _members.Add(member);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+
+}
---
+++ WB6/WB6/Models/DataModels/NTVMember.cs
@@ -0,0 +1,12 @@
+﻿namespace WB6.Models.DataModels
+{
+    public class NTVMember
+    {
+        public string NTVMemberId { get; set; }
+        public string NTVUserName { get; set; }
+        public string NTVEmail { get; set; }
+        public string NTVFullName { get; set; }
+        public string NTVPassword { get; set; }
+
+    }
+}
